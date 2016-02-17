1f0ade4895de643ecc1b8f4e5e6acc50cbeb1c12
marcellushan <mhannah@highlands.edu>
2016-02-16 22:17:43 -0500

    Resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,36 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var education = {
+    "schools": [
+        {   "name": "NAU",
+            "location": "Flagstaff, AZ",
+            "degree": "",
+            "majors": ["Computer Science Engineering"],
+            "date": "1983-1985"
+        },
+            {"name": "UA",
+            "city": "Tucson",
+            "state": "Arizona",
+            "major": "Systems Engineering"
+        },
+         {"name": "UTA",
+            "city": "Arlington",
+            "state": "TX",
+            "major": "Computer Engineering"
+         }
+    ]
+};
+    
+
+
+//console.log(contactInfo);
+
+        
+                }
+    }
+    "cities": ["Flagstaff", "Dallas", "Tucson"],
+    "schools": ["NAU", "UTA"],
+    "graduation year": "2008"
