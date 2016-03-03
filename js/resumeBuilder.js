d70b3b8f4057b7aa8e856214ba1e414ddd7ca201
marcellushan <mhannah@highlands.edu>
2016-03-03 06:56:25 -0500

    Finally getting it

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,116 +2,48 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var work = {
-    "jobs": [
-        {   "employer": "Southern Company",
-            "title": "Systems Analyst",
-            "location": "Atlanta, GA",
-            "dates": "2011-2013",
-            "description": "Web Analytics for various websites"
-        },
-        {   "employer": "Coca-Cola Enterprises",
-            "title": "Systems Administrator",
-            "location": "Atlanta, GA",
-            "dates": "2001-2011",
-            "description": "IIS Web Admin"
-        },
-        {   "employer": "Georgia Highlands College",
-            "title": "Webmaster",
-            "location": "Rome, GA",
-            "dates": "2015-2016",
-            "description": "LAMP Web Developer"
-        }
-    ]
-};
+//var formattedName = HTMLheaderName.replace("%data%", "Marc Hannah");
+//var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
 
 var bio = {
     "name": "Marc Hannah",
     "role": "Web Developer",
-    "welcomeMessage": "Welcome to MarcJHannah.com",
     "contacts": {
-                    "mobile": "404-832-0748",
-                    "email": "marcjhannah@gmail.com",
-                    "github":"marcjhannah",
-                    "twitter": "@marcjhannah",
-                    "location" :"Rome, GA"
+      "mobile": "404-832-0748",
+      "email": "marcjhannah@gmail.com",
+      "github": "marcjhannah",
+      "twitter": "@marcjhannah",
+      "location": "Rome"
     },
-    "skills": ["PHP","javascript","jQuery","mySQL","HTML","CSS"]
-};
-
-var projects = {
-    "project": [
-        {   "title": "Collaboration Cutover",
-            "dates": "2010-2011",
-            "description": "Decouple the collaboration infrastructure",
-            "images": ["marc.jpg","toni.jpg"]
-        }
-    ]
-};
-
-var education = {
-    "schools": [
-        {   "name": "NAU",
-            "location": "Flagstaff, AZ",
-            "degree": "",
-            "majors": ["Computer Science Engineering"],
-            "dates": "1983-1985",
-            "url": "https://www.nau.edu/"
-        },
-            {"name": "UA",
-            "city": "Tucson",
-            "state": "Arizona",
-            "major": "Systems Engineering",
-             "dates": "1981-1983",
-            "url": "http://www.arizona.edu/"
-        },
-         {"name": "UTA",
-            "city": "Arlington",
-            "state": "TX",
-            "major": "Computer Engineering",
-           "dates": "1992-1994",
-            "url": "http://www.arizona.edu/"
-         }
+    "welcomeMessage": "Welcome to my amazing page",
+    "skills": [
+        "PHP","Javascript","jQuery"
     ],
-    "online courses": [
-        {"title": "Google Analytics IQ",
-         "school": "Google Academy",
-         "dates": "2013 - 2014",
-         "url": "https://analyticsacademy.withgoogle.com/course01"
-        }
-    ]
-    
-};
+    "pictureURL": "images/marc.jpg"
+}
 
-var mySkills = bio.skills;
-if(mySkills.length)
-    {
-     $("#header").append(HTMLskillsStart);
-        for(skill in bio.skills)
-            {
-                var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-                $("#skills").append(formattedSkill);
-            }
-    }
-function displayWork()
-    {
-    var myJobs = work.jobs;
-    for(job in myJobs)
-        {
-        $("#workExperience").append(HTMLworkStart);
-        var formattedEmployer = HTMLworkEmployer.replace("%data%",myJobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%",myJobs[job].title);
-        var formattedDates = HTMLworkDates.replace("%data%",myJobs[job].dates);
-        var formattedLocation = HTMLworkLocation.replace("%data%", myJobs[job].location);
-        var formattedDescription = HTMLworkDescription.replace("%data%",myJobs[job].description);
-        $(".work-entry:last").append(formattedEmployer + formattedTitle);
-        $(".work-entry:last").append(formattedDates);
-        $(".work-entry:last").append(formattedLocation);
-        $(".work-entry:last").append(formattedDescription);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
-        };
-    };
-displayWork();
+$("#header").prepend(formattedSkills);
+$("#header").prepend(formattedBioPic);
+$("#header").prepend(formattedLocation);
+$("#header").prepend(formattedTwitter);
+$("#header").prepend(formattedGithub);
+$("#header").prepend(formattedEmail);
+$("#header").prepend(formattedMobile);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
-//console.log(contactInfo);
+$("#mapDiv").append(googleMap);
+
+console.log(formattedRole);
 
